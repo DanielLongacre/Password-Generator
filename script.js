c976d0a5804e99732be0e3f354a176de8aabe5fe
Daniel Longacre <danieljlongacre@gmail.com>
2023-07-16 18:01:27 -0400

    Added array of lower case characters and succesfully looped for that validation only

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var generateBtn = document.querySelector("#generate");
 //Define Generate Password function
 function generatePassword() {
 
+  //Create variable to hold password
+  var password = "";
+
   // 1. Prompt the user for password criteria
   //    a. Password length
   var len = prompt("How many characters would you like your password to contain?");
-  console.log(len);
   if(len < 8 || len > 128) {
     alert("The length of your password must be between 8 and 128 characters. Please try again.");
     return;
@@ -23,21 +25,28 @@ function generatePassword() {
   //   return;
   // }
 
+
+  //Arrays holding the different combinations
+  var lowerOnly = "abcdefghijklmnopqrstuvwxyz";
   
 
   // 2. Validate the input
-  if() {
-
-  } else if () {
-
-  } else if () {
-
+  if( lower === true && upper === false && nums === false && specialChars === false) {
+    for(var i = 0; i < len; i++) {
+      var rand = Math.floor(Math.random() * lowerOnly.length);
+      password += lowerOnly.substring(rand, rand + 1);
+    }
   }
+  // } else if () {
+
+  // } else if () {
+
+  // }
 
 
 
   // 3. Display password to the page.
-  return "Generated Password will go here!";
+  return password;
 }
 
 
