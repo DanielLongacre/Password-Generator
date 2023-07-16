9bac175e6d14642cb9318802bd9e6e1c41c0a972
Daniel Longacre <danieljlongacre@gmail.com>
2023-07-16 16:10:22 -0400

    Created generatePassword function, added sudo-code, returned a string to make sure it worked

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Define Generate Password function
+function generatePassword() {
+  console.log("Button has been clicked!");
+
+  // 1. Prompt the user for password criteria
+  //    a. Password length
+  //    b. Lowercase, Uppercase, numbers, special characters
+  // 2. Validate the input
+  // 3. Display password to the page.
+
+
+
+  
+  return "Generated Password will go here!";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
